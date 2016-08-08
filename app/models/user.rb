8e6cfab7a87093f5b67cbb35ac1e80d6a5c9b83b
yossava <vcckaskus@gmail.com>
2016-08-08 11:35:01 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   mount_uploader :profil_image, ImageUploader
   devise :registerable, :confirmable
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:facebook, :twitter, :google_oauth2, :instagram]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -22,12 +22,18 @@ class User < ActiveRecord::Base
       where("lower(namalengkap) LIKE :search OR lower(email) LIKE :search", search: "%#{search}%")
   end
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.namalengkap = auth.info.name   # assuming the user model has a name
-    user.remote_profil_image_url = auth.info.image.gsub('http://','https://') # assuming the user model has an image
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        if auth.provider == "facebook" || auth.provider == "google_oauth2"
+      user.email = auth.info.email
+          else
+          user.email = "#{auth.info.name.parameterize}@MasukanEmailAnda.com"
+        end
+      user.password = Devise.friendly_token[0,20]
+      user.namalengkap = auth.info.name   # assuming the user model has a name
+      user.remote_profil_image_url = auth.info.image.gsub('http://','https://') # assuming the user model has an image
+      user.skip_confirmation!
+      user.save!
+      end
   end
-end
 
 end
